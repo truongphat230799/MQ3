92201ae6a35466049b56dfc26cc04219f039bcf5
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-06-12 23:52:36 +0700

    Add files via upload

---
+++ definition.js
@@ -0,0 +1,97 @@
+Blockly.Blocks['mq3_get_ppm'] = {
+    init: function() {
+      this.jsonInit(
+        {
+          "message0": "đọc giá trị nồng độ ppm chân %1",
+          "args0": [
+            {
+              "type": "field_dropdown",
+              "name": "NAME",
+              "options": [
+                [
+                  "P0",
+                  "pin0"
+                ],
+                [
+                  "P1",
+                  "pin1"
+                ],
+                [
+                  "P2",
+                  "pin2"
+                ]
+              ]
+            }
+          ],
+          "output": null,
+          "colour": "#6642bf",
+          "helpUrl": ""
+        }
+      );
+    },
+    getDeveloperVars: function() {
+      return ['mq135_citybit'];
+    }
+    
+  };
+  
+  Blockly.Python['mq3_get_acohol'] = function(block) {
+    var dropdown_name = block.getFieldValue('NAME');
+    Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
+    Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin';
+    Blockly.Python.definitions_['import_mq3'] = 'from mq3 import MQ3';
+    Blockly.Python.definitions_["import_create_mq3"] = 'mq3 = MQ3(Pin(' + dropdown_name + '.adc_pin)) # analog PIN';
+    // TODO: Assemble Python into code variable.
+    var code = 'mq3_citybit.get_ppm()';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+  };
+
+  Blockly.Blocks['mq3_get_ppm'] = {
+    init: function() {
+      this.jsonInit(
+        {
+          "message0": "đọc giá trị nồng độ ppm chân %1",
+          "args0": [
+            {
+              "type": "field_dropdown",
+              "name": "NAME",
+              "options": [
+                [
+                  "P0",
+                  "pin0"
+                ],
+                [
+                  "P1",
+                  "pin1"
+                ],
+                [
+                  "P2",
+                  "pin2"
+                ]
+              ]
+            }
+          ],
+          "output": null,
+          "colour": "#6642bf",
+          "helpUrl": ""
+        }
+      );
+    },
+    getDeveloperVars: function() {
+      return ['mq3'];
+    }
+    
+  };
+  
+  Blockly.Python['mq3_get_acohol'] = function(block) {
+    var dropdown_name = block.getFieldValue('NAME');
+    Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
+    Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin';
+    Blockly.Python.definitions_['import_mq3'] = 'from mq3 import MQ3';
+    Blockly.Python.definitions_["import_create_mq3"] = 'mq3 = MQ3(Pin(' + dropdown_name + '.adc_pin)) # analog PIN';
+    // TODO: Assemble Python into code variable.
+    var code = 'mq3_citybit.get_acohol()';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+  };
---
+++ mq3.py
@@ -0,0 +1,56 @@
+import math
+import time
+from machine import ADC, Pin
+
+class MQ3(object):
+    """ Class for dealing with MQ3 Gas Sensors """
+    # The load resistance on the board
+    RLOAD = 10000 # 10.0
+    # Calibration resistance at atmospheric CO2 level
+    RZERO = 800 #76.63 #
+    # Parameters for calculating ppm of CO2 from sensor resistance
+    PARA = 0.3934
+    PARB = -1.504 #2.769034857
+
+    # Atmospheric CO2 level for calibration purposes
+    ATMOACOHOL = 60 #397.13
+
+
+    def __init__(self, pin):
+        self.pin = pin
+
+    
+    def get_resistance(self):
+        """Returns the resistance of the sensor in kOhms // -1 if not value got in pin"""
+        adc = ADC(self.pin)
+        adc.atten(ADC.ATTN_11DB)
+        adc.width(ADC.WIDTH_12BIT)
+        value = adc.read()
+        print('ADC value: ', value)
+        # if value == 0:
+        #     return -1
+        if value < 1:
+            return 1
+        elif value >= 4000:
+            return 4000
+        
+        volt_sensor = (value / 4095) * 3.3
+        return (3.3 * self.RLOAD / volt_sensor) - self.RLOAD
+
+    
+    def get_ppm(self):
+        """Returns the ppm of CO2 sensed (assuming only CO2 in the air)"""
+        #print(self.get_resistance(), self.RZERO, -self.PARB)
+        ratio = self.get_resistance() / 800
+        x = 0.3934 * ratio
+        return ratio * PARA + PARB
+  
+
+    def get_acohol(self):
+        """Returns the resistance RZero of the sensor (in kOhms) for calibration purposes
+        corrected for temperature/humidity"""
+        ratio = self.get_resistance() / 800
+        x = 0.3934 * ratio
+        return math.pow(x,-1.504)
+
+
