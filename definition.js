2809a939d6edf17420f037dfb5945c08b78fd91b
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-06-12 23:57:31 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -30,28 +30,28 @@ Blockly.Blocks['mq3_get_ppm'] = {
       );
     },
     getDeveloperVars: function() {
-      return ['mq135_citybit'];
+      return ['mq3'];
     }
     
   };
   
-  Blockly.Python['mq3_get_acohol'] = function(block) {
+  Blockly.Python['mq3_get_ppm'] = function(block) {
     var dropdown_name = block.getFieldValue('NAME');
     Blockly.Python.definitions_['import_yolobit'] = 'from yolobit import *';
     Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin';
     Blockly.Python.definitions_['import_mq3'] = 'from mq3 import MQ3';
     Blockly.Python.definitions_["import_create_mq3"] = 'mq3 = MQ3(Pin(' + dropdown_name + '.adc_pin)) # analog PIN';
     // TODO: Assemble Python into code variable.
-    var code = 'mq3_citybit.get_ppm()';
+    var code = 'mq3.get_ppm()';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
   };
 
-  Blockly.Blocks['mq3_get_ppm'] = {
+  Blockly.Blocks['mq3_get_acohol'] = {
     init: function() {
       this.jsonInit(
         {
-          "message0": "đọc giá trị nồng độ ppm chân %1",
+          "message0": "đọc giá trị nồng độ cồn chân %1",
           "args0": [
             {
               "type": "field_dropdown",
@@ -91,7 +91,7 @@ Blockly.Blocks['mq3_get_ppm'] = {
     Blockly.Python.definitions_['import_mq3'] = 'from mq3 import MQ3';
     Blockly.Python.definitions_["import_create_mq3"] = 'mq3 = MQ3(Pin(' + dropdown_name + '.adc_pin)) # analog PIN';
     // TODO: Assemble Python into code variable.
-    var code = 'mq3_citybit.get_acohol()';
+    var code = 'mq3.get_acohol()';
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.Python.ORDER_NONE];
   };
